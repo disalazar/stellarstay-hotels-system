df573b14d3ac01c01b81277dc114add6f8d59133
diego.salazar <diegofacha80@gmail.com>
2025-09-24 20:11:40 -0300

    feat(1): Init api

---
+++ src/main/java/com/stellarstay/hotelsystem/StellarstayHotelsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.stellarstay.hotelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarstayHotelsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarstayHotelsSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stellarstay/hotelsystem/StellarstayHotelsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stellarstay.hotelsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarstayHotelsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
