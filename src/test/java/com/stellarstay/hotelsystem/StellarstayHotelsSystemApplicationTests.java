e363ee2bff8aa9b82b91d49b8d58fd1fa72e8c54
disalazar <diegosalazar1980@hotmail.com>
2025-09-25 14:36:35 -0300

    feat(1): init api

--- src/test/java/com/stellarstay/hotelsystem/StellarstayHotelsSystemApplicationTests.java
+++ src/test/java/com/stellarstay/hotelsystem/StellarstayHotelsSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.stellarstay.hotelsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StellarstayHotelsSystemApplicationTests {
 
 	@Test
